08af1a150cc362189f4ba75fb98328e3e77b28fb
Remy Bos <27890746+sjokkateer@users.noreply.github.com>
2021-10-06 14:46:36 +0200

    PROGRAM: Add display menu function

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,26 @@
 #include <Arduino.h>
 
+void init();
+void transmit(uint8_t);
+void transmitLine(const char *);
+uint8_t receive();
+void act(char);
+void displayMenu();
+
+void setup()
+{
+  init();
+}
+
+char data;
+
+void loop()
+{
+  data = receive();
+  act(data);
+}
+
+// USART Functions //
 void init()
 {
   UCSR0C = 0b00000110;
@@ -18,6 +39,16 @@ void transmit(uint8_t data)
   UDR0 = data;
 }
 
+void transmitLine(const char *line)
+{
+  for (unsigned int i = 0; i < strlen(line); i++)
+  {
+    transmit(line[i]);
+  }
+
+  transmit('\n');
+}
+
 uint8_t receive()
 {
   // Polling
@@ -27,17 +58,50 @@ uint8_t receive()
   return UDR0;
 }
 
-void setup()
+// Application Functions //
+// The application is in a waiting state/sleep until it receives S for start.
+void act(char data)
 {
-  init();
+  switch (data)
+  {
+  case 'S':
+  case 's':
+    // Display menu
+    displayMenu();
+    break;
+  case 'D':
+  case 'd':
+    // Show the levels of the digital input pins 8 through 13
+    break;
+  case 'A':
+  case 'a':
+    // Show the levels of the analog input pins 0 through 5
+    break;
+  case 'C':
+  case 'c':
+    // Clear and stop terminal server
+    break;
+  default:
+    break;
+  }
 }
 
-char data;
+const char *MENU_TITLE = "View current input levels:";
 
-void loop()
+int const MENU_SIZE = 3;
+const char *menuOptions[MENU_SIZE] = {
+  "- D: Digital Inputs DI08 - DI13",
+  "- A: Analog Inputs A0 - A5",
+  "- C: Clear Screen"
+};
+
+// Send the menu string character by character
+void displayMenu()
 {
-  // First poll/wait for a character input
-  data = receive();
-  // Then echo the character to terminal client
-  transmit(data);
+  transmitLine(MENU_TITLE);
+  for (uint8_t i = 0; i < MENU_SIZE; i++)
+  {
+    transmitLine(menuOptions[i]);
+  }
+  transmit('\n');
 }
