c763f2c27170aa511259c43e0b7b4d5a6b4a0270
Remy Bos <27890746+sjokkateer@users.noreply.github.com>
2021-11-08 09:05:44 +0100

    TIMER: Add comments

--- src/Timer.cpp
+++ src/Timer.cpp
@@ -2,15 +2,26 @@
 
 void Timer::reset()
 {
+    // Reset the current counter
+    // of timer 2
     TCNT2 = 0;
 }
 
 void Timer::enable()
 {
+    // Timer interrupt flag register
+    // requires us to write '1' to the
+    // bit to reset potential interrupts.
+    TIFR2 = 0 | (1 << OCF2A);
+    // Enable compare match A interrupt
     TIMSK2 = 0 | (1 << OCIE2A);
 }
 
 void Timer::disable()
 {
+    // Disable all interrupts
+    // in this case it will at least
+    // disable the interrupts on compare match A
+    // (assuming I-bit is written '1')
     TIMSK2 = 0;
 }
--- src/main.cpp
+++ src/main.cpp
@@ -22,14 +22,28 @@ Timer *timer;
 
 void setup()
 {
+    Serial.begin(9600);
+
     finalIndex = strlen(TEST_LINE) - 1;
     transmitCharIndex = -1;
 
     // If not dynamically constructed, the timer settings
     // through the constructor did not get set.
     transmitter = new Transmitter(new OutputPin('D', 3));
+
+    // Timer now starts ticking and interrupted
+    // although there would not be a direct reason
+    // for this.
+    // So the interrupt flag might already be set
+    // before we actually care, and immediately get
+    // interrupted
     timer = new Timer();
     timer->reset();
+
+    _delay_ms(1500);
+
+    Serial.println((TIFR2 & OCF2A) >> OCF2A);
+    Serial.println(TCNT2);
 
     sei();
 }
@@ -58,6 +72,10 @@ void loop()
 
 ISR(TIMER2_COMPA_vect)
 {
+    // Interrupt is triggered when sei(),
+    // OCIE2A, and OCF2A are all '1'.
+
+    // Hardware clears the OCF2A flag.
     switch (transmitter->getState())
     {
     case RESETTING:
@@ -65,6 +83,7 @@ ISR(TIMER2_COMPA_vect)
         timer->reset();
 
         transmitter->setState(IDLE);
+        break;
     default:
         transmitter->act(transmitChar);
         break;
